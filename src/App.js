4324ee421b12681e741b5e3dbaac2d70a29e6d35
sumana10 <sumanaonlyme@gmail.com>
2022-06-21 09:34:47 +0530

    classbased todo app

--- src/App.js
+++ src/App.js
@@ -1,94 +1,107 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import logo from "./LCO-logo-white.png";
 import "./App.css";
 
 class App extends Component {
 
-  
-  constructor(props){
-    super(props);
-    this.state = {
-      newItem: "",
-      list: []
-    }
-  }
+  //if we take input from other component then props can only read them
 
-  addItem(todoValue) {
-    
-    if(todoValue !== "") {
-      
+	constructor(props) {
+
+		super(props);
+
+		this.state = {
+			newItem: "",
+			list: [],
+		};
+
+	}
+
+	addItem(todoValue) {
+		
+    if (todoValue !== "") {
+			
       const newItem = {
-        id: Date.now(),
-        value: todoValue
-      }
+				id: Date.now(),
+				value: todoValue,
+			};
+
+			const list = [...this.state.list, newItem];
+			
+			this.setState({
+				list,
+				newItem: "",
+			});
+
+		}
+	}
+
+	deleteItem(id) {
+
+		const list = [...this.state.list];
+
+		const updatedlist = list.filter((item) => item.id !== id);
+
+		this.setState({
+			list: updatedlist,
+		});
+
+	}
+
+	updateInput(input) {
+
+		this.setState({
+			newItem: input,
+		});
     
-      const list = [...this.state.list, newItem]
-      //list.push(newItem);
+	}
 
-       this.setState({
-        list,
-        newItem: ""
-      })
-    }
-  }
-
-  deleteItem(id) {
-   
-    const list = [...this.state.list];
-    
-    const updatedlist = list.filter(item => item.id !== id)
-    
-    this.setState({
-      list: updatedlist
-    })
-  }
-
-  updateInput(input){
-    this.setState({
-      newItem: input
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <img src={logo}
-        width="100" height="100"
-        className="logo"
-        alt="logo"/>
-        <h1 className="app-title">LCO ToDo App</h1>
-        <div className="container">
-          Add an Item....
-          <br />
-          <input type="text" className="input-text"
-          placeholder="Write a Todo" required
-          value={this.state.newItem}
-          onChange={e=>this.updateInput(e.target.value)}
-          />
-          <button className="add-btn" onClick={()=>this.addItem(this.state.newItem)} disabled={!this.state.newItem.length}>Add Todo</button>
-          <div className="list">
-            <ul>
-              {this.state.list.map(item => {
-                return (
-                  <li key={item.id}>
-                    
-                    {item.value}
-                    <button
-                      className="btn"
-                      onClick={() => this.deleteItem(item.id)}
-                    >
-                      Delete
-                    </button>
-                  </li>
-                );
-              })}
-             
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<img src={logo} width="100" height="100" className="logo" alt="logo" />
+				<h1 className="app-title">LCO ToDo App</h1>
+				<div className="container">
+        <form>
+				
+					<input
+						type="text"
+						className="input-text"
+						placeholder="Write a Todo"
+						required
+						value={this.state.newItem}
+						onChange={(e) => this.updateInput(e.target.value)}
+					/>
+					<button
+						className="add-btn"
+						onClick={() => this.addItem(this.state.newItem)}
+						disabled={!this.state.newItem.length}
+					>
+						Add Todo
+					</button>
+          </form>
+					<div className="list">
+						<ul>
+							{this.state.list.map((item) => {
+								return (
+									<li key={item.id}>
+										{item.value}
+										<button
+											className="btn"
+											onClick={() => this.deleteItem(item.id)}
+										>
+											Delete
+										</button>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+        
+			</div>
+		);
+	}
 }
 
 export default App;
